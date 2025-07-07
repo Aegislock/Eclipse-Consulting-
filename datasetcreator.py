a63357a28d69112dcc03cd93bbd86f4c02655b67
Felix Tong <tongfelix000@gmail.com>
2025-07-06 18:23:34 -0700

    TQDM

--- datasetcreator.py
+++ datasetcreator.py
@@ -2,34 +2,38 @@ import cv2
 import bisect
 import random
 import os
+from tqdm import tqdm
 
 class DatasetCreator:
     def __init__(self, dataset_size):
         self.dataset_size = dataset_size
-        self.VIDEOS_PATH = "/Dataset Videos"
-        self.OUTPUT_DIR = "/Dataset"
+        self.VIDEOS_PATH = "Dataset Videos"
+        self.OUTPUT_DIR = "Dataset"
     
     def create_dataset(self):
         video_paths = []
         video_frames = []
         boundaries = []
-        directory = os.getcwd() + self.VIDEOS_PATH
-        for filename in os.listdir(directory):
+
+        directory = os.path.join(os.getcwd(), self.VIDEOS_PATH)
+        for filename in tqdm(os.listdir(directory), desc="Listing video files"):
             video_path = os.path.join(directory, filename)
             video_paths.append(video_path)
-        videos = len(video_paths)
-        if videos:      
-            for video_path in video_paths:
+
+        if video_paths:
+            for video_path in tqdm(video_paths, desc="Getting frame counts"):
                 cap = cv2.VideoCapture(video_path)
                 frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
                 video_frames.append(frames)
                 cap.release()
-            # Create cumulative frame count 
+
             boundaries.append(0)
-            for i in range(len(video_frames)):
+            for i in tqdm(range(len(video_frames)), desc="Building boundaries"):
                 boundaries.append(boundaries[-1] + video_frames[i])
+
             global_frames = random.sample(range(sum(video_frames)), self.dataset_size)
-            for frame in global_frames:
+
+            for frame in tqdm(global_frames, desc="Extracting frames"):
                 index = bisect.bisect_right(boundaries, frame) - 1
                 local_frame = frame - boundaries[index]
                 try:
